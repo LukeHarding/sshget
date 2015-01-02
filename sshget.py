d7d72a5448d81aa7cdb12edb878f67744f13e7cf
LukeHarding <Harding.luke.aaron@gmail.com>
2015-01-01 19:57:04 -0500

    With added features
    Added the shell access, and reordered the questions

--- sshget.py
+++ sshget.py
@@ -1,22 +1,36 @@
-#/bin/python
 
+#/bin/python
 import os
 
-scpTarget = raw_input("Enter The SSH stuff(who@where): ")
+again = 'y'
+while again == 'y':
+    
+    scpTarget = raw_input("Enter The SSH stuff(who@where): ")
+    
+    portQ = raw_input("Do you need an alt port?: ")
 
-fileNeeded = raw_input("Enter the file you want: ")
+    portQ = portQ.lower()
+    if portQ == 'y' or portQ == "yes":
+        port = raw_input("Okay, which port?: ")
+    
+    needShell = raw_input("Do you need a shell to look around?: ")
+    needShell = needShell.lower()
 
-whereGo = raw_input("Where should I put it?: ")
+    if needShell == 'y' or needShell == "yes":
+        if portQ == 'y' or portQ == "yes":
+            os.system("ssh -p" + port + " " + scpTarget)
+        else:
+            os.system("ssh " + scpTarget)
 
-portQ = raw_input("Do you need an alt port?: ")
+    fileNeeded = raw_input("Enter the file you want: ")
 
-portQ = portQ.lower()
+    whereGo = raw_input("Where should I put it?: ")
 
-if portQ == "y" or portQ == "yes":
-    port = raw_input("Enter the port number")
-    os.system("scp -p" + port + " " + scpTarget + ":" + fileNeeded + " " + whereGo)
+    if portQ == "y" or portQ == "yes":
+        os.system("scp -p" + port + " " + scpTarget + ":" + fileNeeded + " " + whereGo)
 
-else:
-    os.system("scp " + scpTarget + ":" + fileNeeded + " "  + whereGo)
-
-print "I got it! \nBye"
+    else:
+        os.system("scp " + scpTarget + ":" + fileNeeded + " "  + whereGo)
+    again = raw_input("Need to go again?(y/n): ")
+    again = again.lower()
+    print "Bye"
